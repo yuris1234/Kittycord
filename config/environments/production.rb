c2d1309ce2a200a75f3adeb941c43cbc56dacaf0
yuris <yls4@cornell.edu>
2024-01-05 15:27:58 -0500

    changed config back to default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,12 +38,12 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
   # config.action_cable.url = "redis://red-claokv6nt67s73f5idrg:6379"
-  config.action_cable.url = "https://kittycord.onrender.com/cable"
-  config.action_cable.allowed_request_origins = ['https://kittycord.onrender.com', %r{http://kittycord.*}]
+  # config.action_cable.url = "https://kittycord.onrender.com/cable"
+  # config.action_cable.allowed_request_origins = ['https://kittycord.onrender.com', %r{http://kittycord.*}]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
-  config.force_ssl = false    
+  # config.force_ssl = false    
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
